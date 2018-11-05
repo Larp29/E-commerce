bb053b5163d6f351499f08efaffe11342661d9f5
Oficina <31423089+Larp29@users.noreply.github.com>
2018-11-04 19:04:28 -0500

    Actualizacion trabajo
    Primer cargue de avance realizado en la empresa

---
+++ frontend/views/modules/header.php
@@ -0,0 +1,26 @@
+<!--=========================
+=            TOP            =
+==========================-->
+
+<div class="container-fluid barraSuperior" id="top">
+	<div class="container">
+		<div class="row">
+			<!-- SOCIAL -->
+			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+				
+			</div>
+
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				<ul>
+					<li><a href="#ModalIngreso" data-toggle="modal">Ingresar</a></li>
+					<li>|</li>
+					<li><a href="#ModalIngreso" data-toggle="modal">Crear una cuenta</a></li>
+				</ul>
+			</div>
+
+		</div>		
+	</div>
+</div>
+
+
+<!--====  End of TOP  ====-->
---
+++ frontend/views/template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="title" content="Tienda Virtual">
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias tenetur labore in placeat delectus qui illo consequuntur, voluptatibus impedit eaque nam assumenda blanditiis rem architecto. Saepe sed nobis, inventore provident!">
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing elit, Molestias tenetur, labore in placeat delectus, qui illo consequuntur, voluptatibus impedit, eaque nam assumenda, blanditiis rem architecto, Saepe sed nobis, inventore provident!">
+	<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+	<script src="js/plugins/jquery.min.js"></script>
+	<script src="js/plugins/bootstrap.min.js"></script>
+	<title>Tienda</title>
+</head>
+<body>
+	<?php  
+
+	/*==============================
+	=            Header            =
+	==============================*/
+	
+	include "modules/header.php";
+	
+	/*=====  End of Header  ======*/
+
+	?>
+</body>
+</html>
